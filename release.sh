2fbc112e1ceea49675555ba928c8d8b14df0032f
yarnb <yarnb@foxmail.com>
2024-04-16 17:03:44 +0800

    chore

--- release.sh
+++ release.sh
@@ -13,7 +13,7 @@ cd "$project_dir" || exit  # Change to the project directory or exit if it fails
 version=$(node -p "require('./package.json').version")
 npm_package_name=$(node -p "require('./package.json').name")
 git add package.json
-git commit -m "release: ${npm_package_name} v${version}"
+git commit -m "release(${npm_package_name}): v${version}"
 git tag "v${version}"
 git push
 git push --tags
