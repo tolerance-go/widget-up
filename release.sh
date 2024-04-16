9d008e6f122b8a977f8a9b73e2401325ae9ad10b
yarnb <yarnb@foxmail.com>
2024-04-16 17:09:56 +0800

    chore

--- release.sh
+++ release.sh
@@ -14,7 +14,7 @@ version=$(node -p "require('./package.json').version")
 npm_package_name=$(node -p "require('./package.json').name")
 git add package.json
 git commit -m "release(${npm_package_name}): v${version}"
-git tag "v${version}"
+git tag "${npm_package_name}-v${version}"
 git push
 git push --tags
 npm publish
