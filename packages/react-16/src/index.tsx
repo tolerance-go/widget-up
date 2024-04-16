e5dc8e026edba040976366eef88635d98e2c4e11
yarnb <yarnb@foxmail.com>
2024-04-16 11:14:17 +0800

    chore: add react 16

---
+++ packages/react-16/src/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useState } from "react";
+
+const Button = () => {
+  const [color, setColor] = useState("red");
+  return (
+    <button
+      style={{
+        color,
+      }}
+      onClick={() => {
+        setColor((prev) => (prev === "red" ? "blue" : "red"));
+      }}
+    >
+      hi button
+    </button>
+  );
+};
+
+export default Button;
