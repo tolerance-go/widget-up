8bcf91660fed90a0161cf9bbe5abe83b2d3d17c8
yarnb <yarnb@foxmail.com>
2024-04-27 13:05:25 +0800

    chore

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -15,8 +15,4 @@ export type {
 export type * from "@/types/form";
 export { EventBus } from "./EventBus";
 
-export { default as tsDeclarationAlias } from "./tsDeclarationAlias";
-export { default as autoExternalDependencies } from "./autoExternalDependencies";
-export { default as peerDependenciesAsExternal } from "./peerDependenciesAsExternal";
-export { default as serveLivereload } from "./serveLivereload";
-export { default as htmlRender } from "./htmlRender";
+export * from "./rollup-plugins";
---
+++ packages/utils/src/rollup-plugins/index.ts
@@ -0,0 +1,5 @@
+export { default as tsDeclarationAlias } from "./tsDeclarationAlias";
+export { default as autoExternalDependencies } from "./autoExternalDependencies";
+export { default as peerDependenciesAsExternal } from "./peerDependenciesAsExternal";
+export { default as serveLivereload } from "./serveLivereload";
+export { default as htmlRender } from "./htmlRender";
