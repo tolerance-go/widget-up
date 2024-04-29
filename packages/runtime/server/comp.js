5d5739c8150ab2e4a44081d1c4ab8d6c5dc9c930
yarnb <yarnb@foxmail.com>
2024-04-29 15:52:26 +0800

    chore

---
+++ packages/runtime/server/comp.js
@@ -0,0 +1,29 @@
+!(function (e, t) {
+    "object" == typeof exports && "undefined" != typeof module
+      ? (module.exports = t(require("react")))
+      : "function" == typeof define && define.amd
+      ? define(["react"], t)
+      : ((e =
+          "undefined" != typeof globalThis ? globalThis : e || self).MyComponent =
+          t(e.React));
+  })(this, function (e) {
+    "use strict";
+    return function () {
+      var t = e.useState("red"),
+        n = t[0],
+        o = t[1];
+      return e.createElement(
+        "button",
+        {
+          style: { color: n },
+          onClick: function () {
+            o(function (e) {
+              return "red" === e ? "blue" : "red";
+            });
+          },
+        },
+        "hi button"
+      );
+    };
+  });
+  
