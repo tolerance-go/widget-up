01f0ecd01b538afd604279fe2769c318ea93b5eb
yarnb <yarnb@foxmail.com>
2024-04-30 00:29:26 +0800

    chore

--- packages/runtime/server/start.js
+++ packages/runtime/server/start.js
@@ -11,16 +11,18 @@ WidgetUpRuntime.start({
           scriptSrc: () => "/comp.react16.js",
           depends: [
             {
-              name: "react",
-              version: "^16.14.0",
-              scriptSrc: (dep) =>
-                `https://unpkg.com/react@${dep.version}/umd/react.development.js`,
-            },
-            {
               name: "react-dom",
               version: "^16.14.0",
               scriptSrc: (dep) =>
                 `https://unpkg.com/react-dom@${dep.version}/umd/react-dom.development.js`,
+              depends: [
+                {
+                  name: "react",
+                  version: "^16.14.0",
+                  scriptSrc: (dep) =>
+                    `https://unpkg.com/react@${dep.version}/umd/react.development.js`,
+                },
+              ],
             },
           ],
         },
