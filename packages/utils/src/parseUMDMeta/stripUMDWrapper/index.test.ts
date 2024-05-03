18a13dd5272d614bc72b63123213dda76e5ffa69
yarnb <yarnb@foxmail.com>
2024-05-04 00:56:04 +0800

    chore

--- packages/utils/src/parseUMDMeta/stripUMDWrapper/index.test.ts
+++ packages/utils/src/parseUMDMeta/stripUMDWrapper/index.test.ts
@@ -222,4 +222,40 @@ describe("clearFactory", () => {
           }"
     `);
   });
+
+  it("should handle complex UMD scripts correctly", () => {
+    const scriptContent = `
+    ( function( global, factory ) {
+
+      "use strict";
+    
+      if ( typeof module === "object" && typeof module.exports === "object" ) {
+    
+        module.exports = global.document ?
+          factory( global, true ) :
+          function( w ) {
+            if ( !w.document ) {
+              throw new Error( "jQuery requires a window with a document" );
+            }
+            return factory( w );
+          };
+      } else {
+        factory( global );
+      }
+    
+    // Pass this if window is not defined yet
+    } )( typeof window !== "undefined" ? window : this, function( window, noGlobal ) {
+    
+    // Edge <= 12 - 13+, Firefox <=18 - 45+, IE 10 - 11, Safari 5.1 - 9+, iOS 6 - 9.1
+    // throw exceptions when non-strict code (e.g., ASP.NET 4.5) accesses strict mode
+    // arguments.callee.caller (trac-13335). But as of jQuery 3.0 (2016), strict mode should be common
+    // enough that all such attempts are guarded in a try block.
+    "use strict";
+
+  } );
+
+      `;
+    const result = stripUMDWrapper({ scriptContent });
+    expect(result).toMatchInlineSnapshot(`""`);
+  });
 });
