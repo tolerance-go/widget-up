3ff6c60edc0285c39299d8295a2c9398715cff3f
yarnb <yarnb@foxmail.com>
2024-05-04 11:24:43 +0800

    chore

--- packages/runtime/server/start.js
+++ packages/runtime/server/start.js
@@ -3,103 +3,109 @@ WidgetUpRuntime.start({
     {
       name: "widget-up-input-react16",
       version: "0.0.0",
-      scriptSrc: () => "/libs/input.react16.wrap.js",
+      scriptSrc: () => "/libs/input.react16.alias-wrap.async-wrap.js",
       depends: [
         {
           name: "@widget-up-demo/react16",
           version: "0.0.0",
-          scriptSrc: () => "/libs/comp.react16.wrap.js",
+          scriptSrc: () => "/libs/comp.react16.alias-wrap.async-wrap.js",
           depends: [
             {
               name: "react-dom",
               version: "16.14.0",
-              scriptSrc: (dep) => `/libs/react-dom.development.wrap.js`,
+              scriptSrc: (dep) =>
+                `/libs/react-dom.development.alias-wrap.async-wrap.js`,
               depends: [
                 {
                   name: "react",
                   version: "16.14.0",
-                  scriptSrc: (dep) => `/libs/react.development.wrap.js`,
+                  scriptSrc: (dep) =>
+                    `/libs/react.development.alias-wrap.async-wrap.js`,
                 },
               ],
             },
             {
               name: "react",
               version: "16.14.0",
-              scriptSrc: (dep) => `/libs/react.development.wrap.js`,
+              scriptSrc: (dep) =>
+                `/libs/react.development.alias-wrap.async-wrap.js`,
             },
           ],
         },
         {
           name: "@widget-up-demo/react16-2",
           version: "0.0.0",
-          scriptSrc: () => "/libs/comp.react16-2.wrap.js",
+          scriptSrc: () => "/libs/comp.react16-2.alias-wrap.async-wrap.js",
           depends: [
             {
               name: "react-dom",
               version: "16.14.0",
-              scriptSrc: (dep) => `/libs/react-dom.development.wrap.js`,
+              scriptSrc: (dep) =>
+                `/libs/react-dom.development.alias-wrap.async-wrap.js`,
               depends: [
                 {
                   name: "react",
                   version: "16.14.0",
-                  scriptSrc: (dep) => `/libs/react.development.wrap.js`,
+                  scriptSrc: (dep) =>
+                    `/libs/react.development.alias-wrap.async-wrap.js`,
                 },
               ],
             },
             {
               name: "react",
               version: "16.14.0",
-              scriptSrc: (dep) => `/libs/react.development.wrap.js`,
+              scriptSrc: (dep) =>
+                `/libs/react.development.alias-wrap.async-wrap.js`,
             },
           ],
         },
       ],
     },
-    // {
-    //   name: "widget-up-input-react18",
-    //   version: "0.0.0",
-    //   scriptSrc: () => "/libs/input.react18.wrap.js",
-    //   depends: [
-    //     {
-    //       name: "@widget-up-demo/react18",
-    //       version: "0.0.0",
-    //       scriptSrc: () => "/libs/comp.react18.wrap.js",
-    //       depends: [
-    //         {
-    //           name: "react-dom",
-    //           version: "18.2.0",
-    //           scriptSrc: (dep) => `/libs/react-dom18.development.wrap.js`,
-    //           depends: [
-    //             {
-    //               name: "react",
-    //               version: "16.14.0",
-    //               scriptSrc: (dep) => `/libs/react.development.wrap.js`,
-    //             },
-    //           ],
-    //         },
-    //         {
-    //           name: "react",
-    //           version: "18.2.0",
-    //           scriptSrc: (dep) => `/libs/react18.development.wrap.js`,
-    //         },
-    //       ],
-    //     },
-    //   ],
-    // },
+    {
+      name: "widget-up-input-react18",
+      version: "0.0.0",
+      scriptSrc: () => "/libs/input.react18.alias-wrap.async-wrap.js",
+      depends: [
+        {
+          name: "@widget-up-demo/react18",
+          version: "0.0.0",
+          scriptSrc: () => "/libs/comp.react18.alias-wrap.async-wrap.js",
+          depends: [
+            {
+              name: "react-dom",
+              version: "18.2.0",
+              scriptSrc: (dep) => `/libs/react-dom18.development.alias-wrap.async-wrap.js`,
+              depends: [
+                {
+                  name: "react",
+                  version: "16.14.0",
+                  scriptSrc: (dep) => `/libs/react.development.alias-wrap.async-wrap.js`,
+                },
+              ],
+            },
+            {
+              name: "react",
+              version: "18.2.0",
+              scriptSrc: (dep) => `/libs/react18.development.alias-wrap.async-wrap.js`,
+            },
+          ],
+        },
+      ],
+    },
     {
       name: "widget-up-input-jquery3",
       version: "0.0.0",
-      scriptSrc: () => "/libs/input.jquery3.wrap.js",
+      scriptSrc: () => "/libs/input.jquery3.alias-wrap.async-wrap.js",
       depends: [
         {
           name: "@widget-up-demo/jquery3",
           version: "0.0.0",
-          scriptSrc: () => "/libs/comp.jquery3.wrap.js",
+          scriptSrc: () => "/libs/comp.jquery3.alias-wrap.async-wrap.js",
           depends: [
             {
               name: "jquery",
               version: "3.7.1",
-              scriptSrc: (dep) => `/libs/jquery.wrap.js`,
+              scriptSrc: (dep) => `/libs/jquery.alias-wrap.async-wrap.js`,
             },
           ],
         },
