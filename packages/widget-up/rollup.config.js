ab685a1fa69007e06703230d679f1a40979620fe
yarnb <yarnb@foxmail.com>
2024-05-09 02:20:24 +0800

    chore

--- packages/widget-up/rollup.config.js
+++ packages/widget-up/rollup.config.js
@@ -3,7 +3,7 @@ import json from "@rollup/plugin-json";
 import resolve from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 import typescript from "@rollup/plugin-typescript";
-import { autoExternalDependencies, deleteDist } from "widget-up-utils";
+import { autoExternalDependencies, deleteDist, tsDeclarationAlias } from "widget-up-utils";
 import alias from "@rollup/plugin-alias";
 
 const isProduction = process.env.NODE_ENV === "production";
@@ -17,23 +17,22 @@ export default {
   plugins: [
     deleteDist({ dist: "dist" }),
     autoExternalDependencies(),
+    json(),
     alias({
       entries: [{ find: "@", replacement: process.cwd() }],
     }),
     resolve({
       preferBuiltins: true,
     }), // 解析 node_modules 中的模块
-    commonjs(), // 转换 CJS -> ESM, 主要是一些 npm 包仍然是 CJS
-    // json(),
+    commonjs({}),
     typescript({
       tsconfig: "tsconfig.build.json",
     }), // TypeScript 支持
     // css({ output: "bundle.css" }), // CSS 支持，将导入的 CSS 文件捆绑到单独的文件
-    // isProduction && terser(), // 生产环境下压缩代码
-    // tsDeclarationAlias(),
-  ].filter(Boolean), // 使用 .filter(Boolean) 去除数组中的 falsy 值，如 undefined 或 false
-  // 告诉 Rollup 'jquery' 是外部依赖，不要打包进来
+    isProduction && terser(), // 生产环境下压缩代码
+    tsDeclarationAlias(),
+  ],
   watch: {
-    include: "src/**", // 监视 src 目录下的所有文件
+    include: "src/**",
   },
 };
