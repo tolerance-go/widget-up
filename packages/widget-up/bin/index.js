12007123ffd40be7853f427128f6cf9ea11e2aa5
yarnb <yarnb@foxmail.com>
2024-05-15 10:33:49 +0800

    chore

--- packages/widget-up/bin/index.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-import { bin } from "../dist/bin.js";
-
-bin();
--- packages/widget-up/src/bin/index.ts
+++ packages/widget-up/src/bin/index.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import { exec } from "child_process";
@@ -12,7 +13,6 @@ const __dirname = dirname(__filename);
 // Construct the path to the Rollup configuration file
 const rollupConfigPath = join(__dirname, "./rollup.config.js");
 const rollupBinPath = join(__dirname, "../node_modules/.bin/rollup");
-
 
 function runRollupCommand(command: string): Promise<void> {
   return new Promise((resolve, reject) => {
@@ -32,46 +32,44 @@ function runRollupCommand(command: string): Promise<void> {
   });
 }
 
-export const bin = () => {
-  yargs(hideBin(process.argv))
-    .scriptName("wup") // 可以设置脚本名称
-    .usage("$0 <cmd> [args]")
-    .command(
-      "build",
-      "Builds the widget for production",
-      () => {},
-      async () => {
-        console.log("Start Building...");
-        try {
-          await runRollupCommand(
-             `${rollupBinPath} -c ${rollupConfigPath} --environment NODE_ENV:production`
-          );
-          process.exit(0);
-        } catch (error) {
-          console.error("Error during the build:", error);
-          process.exit(1);
-        }
+yargs(hideBin(process.argv))
+  .scriptName("wup") // 可以设置脚本名称
+  .usage("$0 <cmd> [args]")
+  .command(
+    "build",
+    "Builds the widget for production",
+    () => {},
+    async () => {
+      console.log("Start Building...");
+      try {
+        await runRollupCommand(
+          `${rollupBinPath} -c ${rollupConfigPath} --environment NODE_ENV:production`
+        );
+        process.exit(0);
+      } catch (error) {
+        console.error("Error during the build:", error);
+        process.exit(1);
       }
-    )
-    .command(
-      "dev",
-      "Starts the widget in development mode with watch",
-      () => {},
-      async () => {
-        console.log("Starting development server...");
-        try {
-          await runRollupCommand(
-             `${rollupBinPath} -c ${rollupConfigPath} --watch --environment NODE_ENV:development`
-          );
-        } catch (error) {
-          console.error("Error during the development build:", error);
-          process.exit(1);
-        }
+    }
+  )
+  .command(
+    "dev",
+    "Starts the widget in development mode with watch",
+    () => {},
+    async () => {
+      console.log("Starting development server...");
+      try {
+        await runRollupCommand(
+          `${rollupBinPath} -c ${rollupConfigPath} --watch --environment NODE_ENV:development`
+        );
+      } catch (error) {
+        console.error("Error during the development build:", error);
+        process.exit(1);
       }
-    )
-    .version(packageJson.version) // 使用package.json中的版本号
-    .alias("version", "v")
-    .help()
-    .alias("help", "h")
-    .parse();
-};
+    }
+  )
+  .version(packageJson.version) // 使用package.json中的版本号
+  .alias("version", "v")
+  .help()
+  .alias("help", "h")
+  .parse();
