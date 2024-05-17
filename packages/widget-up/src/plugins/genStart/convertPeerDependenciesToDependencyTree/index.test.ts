3fe69e5b22ae17403031f55659822a4d135d1f0f
yarnb <yarnb@foxmail.com>
2024-05-17 12:12:41 +0800

    chore

--- packages/widget-up/src/plugins/genStart/convertPeerDependenciesToDependencyTree/index.test.ts
+++ packages/widget-up/src/plugins/genStart/convertPeerDependenciesToDependencyTree/index.test.ts
@@ -1,12 +1,11 @@
-import { DependencyTreeNode } from "widget-up-runtime";
-import { convertPeerDependenciesToDependencyTree } from ".";
 import { PeerDependenciesTree } from "@/src/utils/getPeerDependTree";
-import { DependencyTreeNodeJson } from "@/types";
+import { convertPeerDependenciesToDependencyTree } from ".";
 
 describe("convertPeerDependenciesToDependencyTree", () => {
   it("should correctly convert a simple peer dependencies tree", () => {
     const input: PeerDependenciesTree = {
       react: {
+        name: "react",
         version: {
           exact: "17.0.1",
           range: "^17.0.1",
@@ -14,29 +13,31 @@ describe("convertPeerDependenciesToDependencyTree", () => {
       },
     };
 
-    const expectedOutput: DependencyTreeNodeJson[] = [
-      {
-        name: "react",
-        version: "17.0.1",
-        scriptSrc: expect.any(Function),
-        linkHref: expect.any(Function),
-        depends: undefined,
-      },
-    ];
-
-    const result = convertPeerDependenciesToDependencyTree(input);
-    expect(result).toEqual(expectedOutput);
+    const result = convertPeerDependenciesToDependencyTree(input, {});
+    expect(result).toMatchInlineSnapshot(`
+      [
+        {
+          "depends": undefined,
+          "linkHref": "() => ''",
+          "name": "react",
+          "scriptSrc": "dep => \`/libs/\${dep.name}_\${WidgetUpRuntime.utils.semverToIdentifier(dep.version.exact)}.js\`",
+          "version": "17.0.1",
+        },
+      ]
+    `);
   });
 
   it("should handle nested dependencies", () => {
     const input: PeerDependenciesTree = {
       react: {
+        name: "react",
         version: {
           exact: "17.0.1",
           range: "^17.0.1",
         },
         peerDependencies: {
           "react-dom": {
+            name: "react-dom",
             version: {
               exact: "17.0.1",
               range: "^17.0.1",
@@ -46,25 +47,25 @@ describe("convertPeerDependenciesToDependencyTree", () => {
       },
     };
 
-    const expectedOutput: DependencyTreeNodeJson[] = [
-      {
-        name: "react",
-        version: "17.0.1",
-        scriptSrc: expect.any(Function),
-        linkHref: expect.any(Function),
-        depends: [
-          {
-            name: "react-dom",
-            version: "17.0.1",
-            scriptSrc: expect.any(Function),
-            linkHref: expect.any(Function),
-            depends: undefined,
-          },
-        ],
-      },
-    ];
-
-    const result = convertPeerDependenciesToDependencyTree(input);
-    expect(result).toEqual(expectedOutput);
+    const result = convertPeerDependenciesToDependencyTree(input, {});
+    expect(result).toMatchInlineSnapshot(`
+      [
+        {
+          "depends": [
+            {
+              "depends": undefined,
+              "linkHref": "() => ''",
+              "name": "react-dom",
+              "scriptSrc": "dep => \`/libs/\${dep.name}_\${WidgetUpRuntime.utils.semverToIdentifier(dep.version.exact)}.js\`",
+              "version": "17.0.1",
+            },
+          ],
+          "linkHref": "() => ''",
+          "name": "react",
+          "scriptSrc": "dep => \`/libs/\${dep.name}_\${WidgetUpRuntime.utils.semverToIdentifier(dep.version.exact)}.js\`",
+          "version": "17.0.1",
+        },
+      ]
+    `);
   });
 });
