d6a4a1dc8a7bf740f24f1fa2c5a78daecf957795
yarnb <yarnb@foxmail.com>
2024-05-19 09:18:11 +0800

    chore

---
+++ packages/widget-up/src/managers/envManager/index.ts
@@ -0,0 +1,24 @@
+export class EnvManager {
+  private static instance: EnvManager;
+
+  public BuildEnv: "development" | "production";
+  public BuildEnvIsDev: boolean;
+  public BuildEnvIsProd: boolean;
+  public RuntimeEnv: string | undefined;
+
+  private constructor() {
+    this.BuildEnv = process.env.NODE_ENV as "development" | "production";
+    this.BuildEnvIsDev = this.BuildEnv === "development";
+    this.BuildEnvIsProd = this.BuildEnv === "production";
+    this.RuntimeEnv = process.env.APP_ENV;
+  }
+
+  public static getInstance(): EnvManager {
+    if (!EnvManager.instance) {
+      EnvManager.instance = new EnvManager();
+    }
+    return EnvManager.instance;
+  }
+}
+
+export const envManager = EnvManager.getInstance();
